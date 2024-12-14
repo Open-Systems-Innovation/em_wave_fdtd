1212160e886c61b25469dd4b887a5a04a44078a4
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-12-14 16:37:14 -0500

    first working draft

--- main.py
+++ main.py
@@ -15,42 +15,42 @@ mu = np.pi*4e-7             # [H/m] vacuum permeability
 epsilon = 1 / (mu * c**2)   # [F/m]
 
 # wave source parameters
-frequency = 24000  # 5e9 [Hz]
+frequency = 5e9 # [Hz]
 amplitude = 1.4*1450            # [?]
 wavelength = c / frequency  # [m]
 ps1 = 50  # point source location
 
 # interior grid parameters (not PML)
-domain_nx = 200                     # number of points in x direction
-domain_ny = 200                     # number of points in the y direction
+domain_nx = 100                     # number of points in x direction
+domain_ny = 100                     # number of points in the y direction
 size_x = 0.5                 # total domin size in x direction [m]
 size_y = 0.5                 # total domin size in x direction [m]
 dx = size_x / domain_nx             # grid spacing in x direction [m]
 dy = size_y / domain_ny             # grid spacing in y direction [m]
 
 # PML boundary parameters
-pml_thickness = 5
+pml_thickness = 3
 R_0 = 1e-8  # PML refelction Coefficient
 pml_order = 2
 
 # overall grid parameters (interior + pml)
-nx = domain_nx + 2*pml_thickness
-ny = domain_ny + 2*pml_thickness
+nx = domain_nx + 2*pml_thickness  # number of CELLS in x direction
+ny = domain_ny + 2*pml_thickness  # number of CELLS in y direciton
 nxm1 = nx - 1
 nym1 = ny - 1
 nxp1 = nx + 1
 nyp1 = ny + 1
 
 # boundaries of the non-pml region
-pis = pml_thickness       # PML i index start
-pie = nx - pml_thickness  # PML i index end
-pjs = pml_thickness       # PML j index start
-pje = ny - pml_thickness  # PML j index end
+pis = pml_thickness         # PML i index start
+pie = nxm1 - pml_thickness  # PML i index end
+pjs = pml_thickness         # PML j index start
+pje = nym1 - pml_thickness  # PML j index end
 
 # time stepping parameters
 courant_factor = 0.9
 dt = courant_factor * 1/(c*np.sqrt(1/(dx**2) + 1/(dy**2)))   # based on CFL
-t_final = dt*350                  # final time
+t_final = dt*350             # final time
 t = 0                        # starting time
 
 # log information to the consol
@@ -65,10 +65,14 @@ print(f"wavelength = {wavelength}[m] \nfrequency = {frequency}[Hz]")
 print(f"epsilon = {epsilon}[?] \nmu = {mu}[?]")
 print(f"c = {c}[m/s]\n")
 
-# Initialize Fields
+# Initialize Fields for entire domain (even PML)
+Ez = np.zeros((nxp1, nyp1))
 Hx = np.zeros((nxp1, ny))
 Hy = np.zeros((nx, nyp1))
-Ez = np.zeros((nxp1, nyp1))
+# INITIALIZAION FOR DEBUGGING ONLY
+# Ez = np.arange(nxp1 * nyp1).reshape(10, 10)
+# Hx = np.arange(nxp1 * ny).reshape(nxp1, ny)
+# Hy = np.arange(nx * nyp1).reshape(nx, nyp1)
 
 # Initialize relative material parameters
 eps_rz = np.ones((nxp1, nyp1))
@@ -95,105 +99,90 @@ Chyez = (2 * dt / dx) / (2 * mu_ry * mu + dt * sigma_my)
 
 # Initialize PML fields
 Ezx_xn = np.zeros((pml_thickness, nym1))
-Ezy_xn = np.zeros((pml_thickness, nym1 - 2*pml_thickness))
+Ezy_xn = np.zeros((pml_thickness, nym1 - 2*(pml_thickness)))
 Ezx_xp = np.zeros((pml_thickness, nym1))
-Ezy_xp = np.zeros((pml_thickness, nym1 - 2*pml_thickness))
-Ezx_yn = np.zeros((nxm1 - 2*pml_thickness, pml_thickness))
+Ezy_xp = np.zeros((pml_thickness, nym1 - 2*(pml_thickness)))
+Ezx_yn = np.zeros((nxm1 - 2*(pml_thickness), pml_thickness))
 Ezy_yn = np.zeros((nxm1, pml_thickness))
-Ezx_yp = np.zeros((nxm1 - 2*pml_thickness, pml_thickness))
+Ezx_yp = np.zeros((nxm1 - 2*(pml_thickness), pml_thickness))
 Ezy_yp = np.zeros((nxm1, pml_thickness))
 
-# Initialize PML material parameters
-print('Initializing PML coefficients')
-
-# Assume pml_thickness is the same for all PML layers
+# Initialize PML Coefficients
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# for the xn region
+# sigma_pex_xn stores the values of the sigma values in the PML cells
 sigma_pex_xn = np.zeros((pml_thickness, nym1))
 sigma_pmx_xn = np.zeros((pml_thickness, nym1))
-
-sigma_max = -(pml_order + 1) * epsilon * c * np.log(R_0) / (2 * dx * pml_thickness)
-rho_e = (np.arange(pml_thickness, 0, -1) - 0.75) / pml_thickness
-rho_m = (np.arange(pml_thickness, 0, -1) - 0.25) / pml_thickness
-for i in range(pml_thickness):
-    sigma_pex_xn[i, :] = sigma_max * rho_e[i] ** pml_order
-    sigma_pmx_xn[i, :] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
-
-# Coefficients updating Hy
-Chyh_xn = (2 * mu - dt * sigma_pmx_xn) / (2 * mu + dt * sigma_pmx_xn)
-Chyez_xn = (2 * dt / dx) / (2 * mu + dt * sigma_pmx_xn)
-
-# Coefficients updating Ezx
-Cezxe_xn = (2 * epsilon - dt * sigma_pex_xn) / (2 * epsilon + dt * sigma_pex_xn)
-Cezxhy_xn = (2 * dt / dx) / (2 * epsilon + dt * sigma_pex_xn)
-
-# Coefficients updating Ezy
-Cezye_xn = 1
-Cezyhx_xn = -dt / (dy * epsilon)
-
 # For the xp region (PML layer in the positive x direction)
 sigma_pex_xp = np.zeros((pml_thickness, nym1))
 sigma_pmx_xp = np.zeros((pml_thickness, nym1))
-
-sigma_max = -(pml_order + 1) * epsilon * c * np.log(R_0) / (2 * dx * pml_thickness)
-rho_e = (np.arange(1, pml_thickness + 1) - 0.75) / pml_thickness
-rho_m = (np.arange(1, pml_thickness + 1) - 0.25) / pml_thickness
-for i in range(pml_thickness):
-    sigma_pex_xp[i, :] = sigma_max * rho_e[i] ** pml_order
-    sigma_pmx_xp[i, :] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
-
-# Coefficients updating Hy
-Chyh_xp = (2 * mu - dt * sigma_pmx_xp) / (2 * mu + dt * sigma_pmx_xp)
-Chyez_xp = (2 * dt / dx) / (2 * mu + dt * sigma_pmx_xp)
-
-# Coefficients updating Ezx
-Cezxe_xp = (2 * epsilon - dt * sigma_pex_xp) / (2 * epsilon + dt * sigma_pex_xp)
-Cezxhy_xp = (2 * dt / dx) / (2 * epsilon + dt * sigma_pex_xp)
-
-# Coefficients updating Ezy
-Cezye_xp = 1
-Cezyhx_xp = -dt / (dy * epsilon)
-
 # For the yn region (PML layer in the negative y direction)
 sigma_pey_yn = np.zeros((nxm1, pml_thickness))
 sigma_pmy_yn = np.zeros((nxm1, pml_thickness))
-
-sigma_max = -(pml_order + 1) * epsilon * c * np.log(R_0) / (2 * dy * pml_thickness)
-rho_e = (np.arange(pml_thickness, 0, -1) - 0.75) / pml_thickness
-rho_m = (np.arange(pml_thickness, 0, -1) - 0.25) / pml_thickness
-for i in range(pml_thickness):
-    sigma_pey_yn[:, i] = sigma_max * rho_e[i] ** pml_order
-    sigma_pmy_yn[:, i] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
-
-# Coefficients updating Hx
-Chxh_yn = (2 * mu - dt * sigma_pmy_yn) / (2 * mu + dt * sigma_pmy_yn)
-Chxez_yn = -(2 * dt / dy) / (2 * mu + dt * sigma_pmy_yn)
-
-# Coefficients updating Ezx
-Cezxe_yn = 1
-Cezxhy_yn = dt / (dx * epsilon)
-
-# Coefficients updating Ezy
-Cezye_yn = (2 * epsilon - dt * sigma_pey_yn) / (2 * epsilon + dt * sigma_pey_yn)
-Cezyhx_yn = -(2 * dt / dy) / (2 * epsilon + dt * sigma_pey_yn)
-
 # For the yp region (PML layer in the positive y direction)
 sigma_pey_yp = np.zeros((nxm1, pml_thickness))
 sigma_pmy_yp = np.zeros((nxm1, pml_thickness))
 
-sigma_max = -(pml_order + 1) * epsilon * c * np.log(R_0) / (2 * dy * pml_thickness)
-rho_e = (np.arange(1, pml_thickness + 1) - 0.75) / pml_thickness
-rho_m = (np.arange(1, pml_thickness + 1) - 0.25) / pml_thickness
+# max sigma value
+sigma_max = -(pml_order + 1) * epsilon * c * np.log(R_0) / (2 * dx * pml_thickness)
+# calculated rho and sigma values (distance from the interior boundary
+# of the PML
+rho_e = (np.arange(pml_thickness, 0, -1) - 0.75) / pml_thickness
+rho_m = (np.arange(pml_thickness, 0, -1) - 0.25) / pml_thickness
 for i in range(pml_thickness):
+    # for xn
+    sigma_pex_xn[i, :] = sigma_max * rho_e[i] ** pml_order
+    sigma_pmx_xn[i, :] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
+    # for yn
+    sigma_pey_yn[:, i] = sigma_max * rho_e[i] ** pml_order
+    sigma_pmy_yn[:, i] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
+
+rho_e = (np.arange(0, pml_thickness) - 0.75) / pml_thickness
+rho_m = (np.arange(0, pml_thickness) - 0.25) / pml_thickness
+for i in range(pml_thickness):
+    # calcualte the actual values of sigma_p
+    # for xp
+    sigma_pex_xp[i, :] = sigma_max * rho_e[i] ** pml_order
+    sigma_pmx_xp[i, :] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
+    # for yp
     sigma_pey_yp[:, i] = sigma_max * rho_e[i] ** pml_order
     sigma_pmy_yp[:, i] = (mu / epsilon) * sigma_max * rho_m[i] ** pml_order
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+# Update coefficients for PML layer
+# Coefficients updating Hy
+Chyh_xn = (2 * mu - dt * sigma_pmx_xn) / (2 * mu + dt * sigma_pmx_xn)
+Chyez_xn = (2 * dt / dx) / (2 * mu + dt * sigma_pmx_xn)
+# Coefficients updating Ezx
+Cezxe_xn = (2 * epsilon - dt * sigma_pex_xn) / (2 * epsilon + dt * sigma_pex_xn)
+Cezxhy_xn = (2 * dt / dx) / (2 * epsilon + dt * sigma_pex_xn)
+# Coefficients updating Ezy
+Cezye_xn = 1
+Cezyhx_xn = -dt / (dy * epsilon)
+# Coefficients updating Hy
+Chyh_xp = (2 * mu - dt * sigma_pmx_xp) / (2 * mu + dt * sigma_pmx_xp)
+Chyez_xp = (2 * dt / dx) / (2 * mu + dt * sigma_pmx_xp)
+# Coefficients updating Ezx
+Cezxe_xp = (2 * epsilon - dt * sigma_pex_xp) / (2 * epsilon + dt * sigma_pex_xp)
+Cezxhy_xp = (2 * dt / dx) / (2 * epsilon + dt * sigma_pex_xp)
+# Coefficients updating Ezy
+Cezye_xp = 1
+Cezyhx_xp = -dt / (dy * epsilon)
+# Coefficients updating Hx
+Chxh_yn = (2 * mu - dt * sigma_pmy_yn) / (2 * mu + dt * sigma_pmy_yn)
+Chxez_yn = -(2 * dt / dy) / (2 * mu + dt * sigma_pmy_yn)
+# Coefficients updating Ezx
+Cezxe_yn = 1
+Cezxhy_yn = dt / (dx * epsilon)
+# Coefficients updating Ezy
+Cezye_yn = (2 * epsilon - dt * sigma_pey_yn) / (2 * epsilon + dt * sigma_pey_yn)
+Cezyhx_yn = -(2 * dt / dy) / (2 * epsilon + dt * sigma_pey_yn)
 # Coefficients updating Hx
 Chxh_yp = (2 * mu - dt * sigma_pmy_yp) / (2 * mu + dt * sigma_pmy_yp)
 Chxez_yp = -(2 * dt / dy) / (2 * mu + dt * sigma_pmy_yp)
-
 # Coefficients updating Ezx
 Cezxe_yp = 1
 Cezxhy_yp = dt / (dx * epsilon)
-
 # Coefficients updating Ezy
 Cezye_yp = (2 * epsilon - dt * sigma_pey_yp) / (2 * epsilon + dt * sigma_pey_yp)
 Cezyhx_yp = -(2 * dt / dy) / (2 * epsilon + dt * sigma_pey_yp)
@@ -207,11 +196,9 @@ def plot_solution(cmap="viridis", output_path="figures/plot.png"):
     fig, axs = plt.subplots(1, 3)
     datasets = [Ez, Hx, Hy]
 
-    amplitude = 1
-
     matricies = []
     for ax, data in zip(axs.flat, datasets):
-        matricies.append(ax.imshow(data, vmin=-amplitude, vmax=amplitude))
+        matricies.append(ax.imshow(data))#, vmin=-amplitude, vmax=amplitude))
 
     fig.colorbar(matricies[0], ax=axs, orientation='horizontal', fraction=.1)
     plt.savefig(output_path, dpi=300)
@@ -220,70 +207,73 @@ def plot_solution(cmap="viridis", output_path="figures/plot.png"):
 
 timestep = 0
 while t < t_final:
+    t += dt
+    ps = pml_thickness
+    pe = -pml_thickness
+    # update Hx
+    Hx[:, ps:pe] = Chxh[:, ps:pe] * Hx[:, ps:pe] + \
+        Chxez[:, ps:pe] * (Ez[:, ps+1:pe] - Ez[:, ps:pe-1])
 
-    # update_Hx()
-    Hx[:, pjs:pje-1] = Chxh[:, pjs:pje-1] * Hx[:, pjs:pje-1] + \
-        Chxez[:, pjs:pje-1] * (Ez[:, pjs+1:pje] - Ez[:, pjs:pje-1])
-    # update_Hy()
-    Hy[pis:pie-1, :] = Chyh[pis:pie-1, :] * Hy[pis:pie-1, :] + \
-        Chyez[pis:pie-1, :] * (Ez[pis+1:pie, :] - Ez[pis:pie-1, :])
-    # update Hx and Hy PML layers
+    # update Hy
+    Hy[ps:pe, :] = Chyh[ps:pe, :] * Hy[ps:pe, :] + \
+        Chyez[ps:pe, :] * (Ez[ps+1:pe, :] - Ez[ps:pe-1, :])
+    # update H in PML layers
     # for xn
-    Hy[0:pis-1, 1:ny] = Chyh_xn * Hy[0:pis-1, 1:ny] + Chyez_xn * \
-        (Ez[1:pis, 1:ny] - Ez[0:pis-1, 1:ny])
+    Hy[:ps, 1:-1] = \
+        Chyh_xn * Hy[:ps, 1:-1] + \
+        Chyez_xn * (Ez[1:ps+1, 1:-1] - Ez[:ps, 1:-1])
     # for xp
-    Hy[pie:nx, 1:ny] = Chyh_xp * Hy[pie:nx, 1:ny] + Chyez_xp * \
-        (Ez[pie+1:nxp1, 1:ny] - Ez[pie:nx, 1:ny])
+    Hy[pe:, 1:-1] = \
+        Chyh_xp * Hy[pe:nx, 1:-1] + \
+        Chyez_xp * (Ez[pe:, 1:-1] - Ez[pe-1:-1, 1:-1])
     # for yn
-    Hx[1:nx, 0:pjs-1] = Chxh_yn * Hx[1:nx, 0:pjs-1] + Chxez_yn * \
-        (Ez[1:nx, 1:pjs] - Ez[1:nx, 0:pjs-1])
+    Hx[1:-1, :ps] = \
+        Chxh_yn * Hx[1:-1, :ps] + \
+        Chxez_yn * (Ez[1:-1, 1:ps+1] - Ez[1:-1, :ps])
     # for yp
-    Hx[1:nx, pje:ny] = Chxh_yp * Hx[1:nx, pje:ny] + Chxez_yp * \
-        (Ez[1:nx, pje+1:nyp1] - Ez[1:nx, pje:ny])
-
+    Hx[1:-1, pe:] = \
+        Chxh_yp * Hx[1:-1, pe:] + \
+        Chxez_yp * (Ez[1:-1, pe:] - Ez[1:-1, pe-1:-1])
     # update_Ez()
-    Ez[pis:pie-1, pjs:pje-1] = (
-        Ceze[pis:pie-1, pjs:pje-1] * Ez[pis:pie-1, pjs:pje-1] +
-        Cezhy[pis:pie-1, pjs:pje-1] *
-        (Hy[pis:pie-1, pjs:pje-1] - Hy[pis-1:pie-2, pjs:pje-1]) +
-        Cezhx[pis:pie-1, pjs:pje-1] *
-        (Hx[pis:pie-1, pjs:pje-1] - Hx[pis:pie-1, pjs-1:pje-2])
+    Ez[ps+1:pe-1, ps+1:pe-1] = (
+        Ceze[ps+1:pe-1, ps+1:pe-1] * Ez[ps+1:pe-1, ps+1:pe-1] +
+        Cezhy[ps+1:pe-1, ps+1:pe-1] *
+        (Hy[ps+1:pe, ps+1:pe-1] - Hy[ps:pe-1, ps+1:pe-1]) +
+        Cezhx[ps+1:pe-1, ps+1:pe-1] *
+        (Hx[ps+1:pe-1, ps+1:pe] - Hx[ps+1:pe-1, ps:pe-1])
     )
 
     # update_impressed_J()
     Cezj = -(2*dt) / (2 * eps_rz[ps1, ps1] * epsilon + dt * sigma_ez[ps1, ps1])
     Ez[ps1, ps1] = Ez[ps1, ps1] + Cezj * np.sin(2 * np.pi * frequency * t)
-    #update_Ez_pml()
+    # update_Ez_PML layers
     # For xn PML region
-    Ezx_xn = Cezxe_xn * Ezx_xn + Cezxhy_xn * (Hy[1:pis, 1:ny] - Hy[0:pis-1, 1:ny])
-    Ezy_xn = Cezye_xn * Ezy_xn + Cezyhx_xn * (Hx[1:pis, pjs+1:pje-1] - Hx[1:pis, pjs:pje-2])
-
+    Ezx_xn = Cezxe_xn * Ezx_xn + Cezxhy_xn * (Hy[1:ps+1, 1:-1] - Hy[0:ps, 1:-1])
+    Ezy_xn = Cezye_xn * Ezy_xn + Cezyhx_xn * (Hx[1:ps+1, ps+1:pe] - Hx[1:ps+1, ps:pe-1])
     # For xp PML region
-    Ezx_xp = Cezxe_xp * Ezx_xp + Cezxhy_xp * (Hy[pie:nx, 1:ny] - Hy[pie-1:nx-1, 1:ny])
-    Ezy_xp = Cezye_xp * Ezy_xp + Cezyhx_xp * (Hx[pie:nx, pjs+1:pje-1] - Hx[pie:nx, pjs:pje-2])
-
+    Ezx_xp = Cezxe_xp * Ezx_xp + Cezxhy_xp * (Hy[pe:, 1:-1] - Hy[pe-1:-1, 1:-1])
+    Ezy_xp = Cezye_xp * Ezy_xp + Cezyhx_xp * (Hx[pe:, ps+1:pe] - Hx[pe:, ps:pe-1])
     # For yn PML region
-    Ezx_yn = Cezxe_yn * Ezx_yn + Cezxhy_yn * (Hy[pis+1:pie-1, 1:pis] - Hy[pis:pie-2, 1:pjs])
-    Ezy_yn = Cezye_yn * Ezy_yn + Cezyhx_yn * (Hx[1:nx, 1:pjs] - Hx[1:nx, 0:pjs-1])
-
+    Ezx_yn = Cezxe_yn * Ezx_yn + Cezxhy_yn * (Hy[ps+1:pe, 1:ps+1] - Hy[ps:pe-1, 1:ps+1])
+    Ezy_yn = Cezye_yn * Ezy_yn + Cezyhx_yn * (Hx[1:-1, 1:ps+1] - Hx[1:-1, :ps])
     # For yp PML region
-    Ezx_yp = Cezxe_yp * Ezx_yp + Cezxhy_yp * (Hy[pis+1:pie-1, pje:ny] - Hy[pis:pie-2, pje:ny])
-    Ezy_yp = Cezye_yp * Ezy_yp + Cezyhx_yp * (Hx[1:nx, pje:ny] - Hx[1:nx, pje-1:ny-1])
-
+    Ezx_yp = Cezxe_yp * Ezx_yp + Cezxhy_yp * (Hy[ps+1:pe, pe:] - Hy[ps:pe-1, pe:])
+    Ezy_yp = Cezye_yp * Ezy_yp + Cezyhx_yp * (Hx[1:-1, pe:] - Hx[1:-1, pe-1:-1])
     # Update the Ez field at the corresponding regions
-    Ez[1:pis, 1:pjs] = Ezx_xn[:, 1:pjs-1] + Ezy_yn[1:pis-1, :]
-    Ez[1:pis, pje:ny] = Ezx_xn[:, pje-1:nym1] + Ezy_yp[1:pis-1, :]
-    Ez[pie:nx, pje:ny] = Ezx_xp[:, pje-1:nym1] + Ezy_yp[pie-1:nxm1, :]
-    Ez[pie:nx, 1:pjs] = Ezx_xp[:, 1:pjs-1] + Ezy_yn[pie-1:nxm1, :]
-    Ez[pis+1:pie-1, 1:pjs] = Ezx_yn + Ezy_yn[pis:pie-2, :]
-    Ez[pis+1:pie-1, pje:ny] = Ezx_yp + Ezy_yp[pis:pie-2, :]
-    Ez[1:pis, pjs+1:pje-1] = Ezx_xn[:, pjs:pje-2] + Ezy_xn
-    Ez[pie:nx, pjs+1:pje-1] = Ezx_xp[:, pjs:pje-2] + Ezy_xp
+    Ez[1:ps+1, 1:ps+1] = Ezx_xn[:, 1:ps+1] + Ezy_yn[1:ps+1, :]  # lower L corner
+    Ez[1:ps+1, pe:] = Ezx_xn[:, pe:] + Ezy_yp[:ps, :]  # upper L corner
+    Ez[pe:, pe:] = Ezx_xp[:, pe:] + Ezy_yp[pe:, :]  # upper R corner
+    Ez[pe:, :ps] = Ezx_xp[:, :ps] + Ezy_yn[pe:, :]    # lower R corner
+    Ez[ps+1:pe-1, :ps] = Ezx_yn + Ezy_yn[ps:pe, :]  # bottom
+    Ez[ps+1:pe-1, pe:] = Ezx_yp + Ezy_yp[ps:pe, :]   # top
+    Ez[:ps, ps+1:pe-1] = Ezx_xn[:, ps:pe] + Ezy_xn  # left
+    Ez[pe:, ps+1:pe-1] = Ezx_xp[:, ps:pe] + Ezy_xp   # right
     
     # plot solution
-    if timestep % 10 == 0:
+    if 0 == 0:
         output_path = f"{output_dir}/timestep_{timestep:04d}.png"
         plot_solution(output_path=output_path)
 
     # increment timestep
     timestep += 1
+    
